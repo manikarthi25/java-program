beace8752dd329a8002d2bd98221cb02f0dc3721
manikarthi25 <manikarthi25@gmail.com>
2022-02-08 18:53:27 +0530

    finding frequency

--- java-programs/src/main/java/com/java/program/interview/FindingDuplication.java
+++ java-programs/src/main/java/com/java/program/interview/FindingDuplication.java
@@ -1,8 +1,50 @@
 package com.java.program.interview;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindingDuplication {
 	
+	public static void findFrequencyOfDuplicationStrings(String word) {
+		
+		String[] words = word.split(" ");
+		Map<String, Integer> map = new HashMap<>();
+		
+		for(String str : words) {
+			if(map.get(str) != null) {
+				map.put(str, map.get(str) + 1);
+			} else {
+				map.put(str, 1);
+			}
+			
+		}
+		
+		System.out.println("find frequency of Duplication String :" + map);
+		
+	}
+	
+public static void findFrequencyOfDuplicationCharacters(String word) {
+		
+		Map<Character, Integer> map = new HashMap<>();
+		
+		for(int i = 0; i< word.length(); i++) {
+			Character c = word.charAt(i);
+			if(map.get(c) != null) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+			
+		}
+		
+		System.out.println("find frequency of Duplication Characters :" + map);
+		
+	}
+	
 	public static void main(String a[]) {
+		
+		findFrequencyOfDuplicationStrings("Mani Mani Mohi Mahi Mohi");
+		findFrequencyOfDuplicationCharacters("Manikandan");
 		
 	}
 
